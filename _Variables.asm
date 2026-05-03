542f4de631227ee51a4e95eb528980172c27e559
Player03Knux <135396707+Player03Knux@users.noreply.github.com>
2026-05-02 20:54:50 -0400

    Spindash code wip

--- _Variables.asm
+++ _Variables.asm
@@ -285,7 +285,11 @@ v_ringbonus:		ds.w	1		; ring bonus at the end of an act
 f_endactbonus:		ds.b	1		; time/ring bonus update flag at the end of an act
 v_sonicend:		ds.b	1		; routine counter for Sonic in the ending sequence
 v_lz_deform:		ds.w	1		; LZ deformation offset, in units of $80
-			ds.b	6		; unused
+Horiz_scroll_delay_val:		ds.w	1	; if its value is a, where a != 0, X scrolling will be based on the player's X position a-1 frames ago
+			ds.b	1		; unused
+			ds.b	1		; unused
+			ds.b	1		; unused
+			ds.b	1		; unused
 f_switch:		ds.b	$10		; flags set when Sonic stands on a switch
 v_scroll_block_1_size:	ds.w	1
 v_scroll_block_2_size:	ds.w	1		; unused
--- _incObj/01 Sonic.asm
+++ _incObj/01 Sonic.asm
@@ -281,6 +281,7 @@ Sonic_Water:
 
 ; Obj01_MdNormal:
 Sonic_MdNormal:	; While Sonic is on the ground and not rolling
+		bsr.w	Sonic_CheckSpindash
 		bsr.w	Sonic_Jump				; check if we need to jump
 		bsr.w	Sonic_SlopeResistWalk			; handle resistance from running up slopes
 		bsr.w	Sonic_Move				; handle Sonic's left/right movement
@@ -1218,6 +1219,149 @@ Sonic_JumpHeight:
 	endif
 		rts						; return
 ; End of function Sonic_JumpHeight
+
+; ---------------------------------------------------------------------------
+; Subroutine to check for starting to charge a spindash
+; ---------------------------------------------------------------------------
+
+AniIDSonAni_Spindash: equ id_Roll
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+; loc_1AC3E:
+Sonic_CheckSpindash:
+	tst.b	spindash_flag(a0)
+	bne.s	Sonic_UpdateSpindash
+	cmpi.b	#id_Duck,obAnim(a0)
+	bne.s	return_1AC8C
+	move.b	(v_jpadpress2).w,d0
+	andi.b	#btnABC,d0
+	beq.w	return_1AC8C
+	move.b	#AniIDSonAni_Spindash,obAnim(a0)
+;	move.w	#SndID_SpindashRev,d0
+;	jsr	(PlaySound).l
+	addq.l	#4,sp
+	move.b	#1,spindash_flag(a0)
+	move.w	#0,spindash_counter(a0)
+;	cmpi.b	#12,air_left(a0)	; if he's drowning, branch to not make dust
+;	blo.s	+
+;	move.b	#2,(Sonic_Dust+anim).w
+;+
+	bsr.w	Sonic_LevelBound
+	bsr.w	Sonic_AnglePos
+
+return_1AC8C:
+	rts
+; End of subroutine Sonic_CheckSpindash
+
+
+; ---------------------------------------------------------------------------
+; Subrouting to update an already-charging spindash
+; ---------------------------------------------------------------------------
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+; loc_1AC8E:
+Sonic_UpdateSpindash:
+	move.b	(v_jpadhold2).w,d0
+	btst	#bitDn,d0
+	bne.w	Sonic_ChargingSpindash
+
+	; unleash the charged spindash and start rolling quickly:
+	move.b	#$E,obHeight(a0)
+	move.b	#7,obWidth(a0)
+	move.b	#id_Roll,obAnim(a0)
+	addq.w	#5,obY(a0)	; add the difference between Sonic's rolling and standing heights
+	move.b	#0,spindash_flag(a0)
+	moveq	#0,d0
+	move.b	spindash_counter(a0),d0
+	add.w	d0,d0
+	move.w	SpindashSpeeds(pc,d0.w),obInertia(a0)
+;	tst.b	(Super_Sonic_flag).w
+;	beq.s	+
+;	move.w	SpindashSpeedsSuper(pc,d0.w),obInertia(a0)
+;+
+	; Determine how long to lag the camera for.
+	; Notably, the faster Sonic goes, the less the camera lags.
+	; This is seemingly to prevent Sonic from going off-screen.
+	move.w	obInertia(a0),d0
+	subi.w	#$800,d0 ; $800 is the lowest spin dash speed
+	add.w	d0,d0
+	andi.w	#$1F00,d0 ; This line is not necessary, as none of the removed bits are ever set in the first place
+	neg.w	d0
+	addi.w	#$2000,d0
+	move.w	d0,(Horiz_scroll_delay_val).w
+
+	btst	#0,obStatus(a0)
+	beq.s	.undefined1
+	neg.w	obInertia(a0)
+	
+.undefined1:
+	bset	#2,obStatus(a0)
+;	move.b	#0,(Sonic_Dust+anim).w
+;	move.w	#SndID_SpindashRelease,d0	; spindash zoom sound
+;	jsr	(PlaySound).l
+	bra.s	Sonic_Spindash_ResetScr
+; ===========================================================================
+; word_1AD0C:
+SpindashSpeeds:
+	dc.w  $800	; 0
+	dc.w  $880	; 1
+	dc.w  $900	; 2
+	dc.w  $980	; 3
+	dc.w  $A00	; 4
+	dc.w  $A80	; 5
+	dc.w  $B00	; 6
+	dc.w  $B80	; 7
+	dc.w  $C00	; 8
+; word_1AD1E:
+SpindashSpeedsSuper:
+	dc.w  $B00	; 0
+	dc.w  $B80	; 1
+	dc.w  $C00	; 2
+	dc.w  $C80	; 3
+	dc.w  $D00	; 4
+	dc.w  $D80	; 5
+	dc.w  $E00	; 6
+	dc.w  $E80	; 7
+	dc.w  $F00	; 8
+; ===========================================================================
+; loc_1AD30:
+Sonic_ChargingSpindash:			; If still charging the dash...
+	tst.w	spindash_counter(a0)
+	beq.s	.undefined2
+	move.w	spindash_counter(a0),d0
+	lsr.w	#5,d0
+	sub.w	d0,spindash_counter(a0)
+	bcc.s	.undefined2
+	move.w	#0,spindash_counter(a0)
+	
+.undefined2:
+	move.b	(v_jpadpress2).w,d0
+	andi.b	#btnABC,d0
+	beq.w	Sonic_Spindash_ResetScr
+	move.w	#(AniIDSonAni_Spindash<<8)|(id_Walk<<0),obAnim(a0)
+;	move.w	#SndID_SpindashRev,d0
+;	jsr	(PlaySound).l
+	addi.w	#$200,spindash_counter(a0)
+	cmpi.w	#$800,spindash_counter(a0)
+	blo.s	Sonic_Spindash_ResetScr
+	move.w	#$800,spindash_counter(a0)
+
+; loc_1AD78:
+Sonic_Spindash_ResetScr:
+	addq.l	#4,sp
+	cmpi.w	#(224/2)-16,(v_lookshift).w
+	beq.s	loc_1AD8C
+	bhs.s	.undefined3
+	addq.w	#4,(v_lookshift).w
+.undefined3:	subq.w	#2,(v_lookshift).w
+
+loc_1AD8C:
+	bsr.w	Sonic_LevelBound
+	bsr.w	Sonic_AnglePos
+	rts
+; End of subroutine Sonic_UpdateSpindash
 
 
 ; ===========================================================================
